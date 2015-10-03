4750a6ac867f517a5f6b7647229b4336173be2bf
liberation_box <liberation.box@gmail.com>
2015-10-03 18:28:25 +0000

    added new artisan command users:show

---
+++ app/Console/Commands/ShowUsers.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User as UserModel;
+use Illuminate\Console\Command;
+
+
+class ShowUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'users:show {gender? : The gender (male/female) of the users}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Show all registered users.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $gender = $this->argument('gender');
+        
+        if (is_null($gender)) {
+            $users = UserModel::all(['id', 'first_name', 'last_name', 'gender', 'email']);    
+        }
+        elseif (in_array($gender, ['male', 'female'])) {
+            $users = UserModel::where('gender', $gender)->select('id', 'first_name', 'last_name', 'gender', 'email')->get();
+        }
+        else {
+            return $this->error('Received invalid argument: ' . $gender);
+        }
+        
+        $this->table(['id', 'First Name', 'Last Name', 'Gender', 'Email'], $users);
+    }
+}
