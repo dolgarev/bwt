213eeea2ac8d5b96b3e4024f1b132d714ebf6dd1
liberation_box <liberation.box@gmail.com>
2015-10-04 19:07:53 +0000

    added foundation5 via CDN

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,11 +4,13 @@
         <title>App Name - @yield('title')</title>
         
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="http://cdn.foundation5.zurb.com/foundation.css" rel="stylesheet" type="text/css">
         <link href="css/app.css" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div class="container">
             @yield('content')
         </div>
+        <script type="text/javascript" src="http://cdn.foundation5.zurb.com/foundation.js"></script>
     </body>
 </html>
