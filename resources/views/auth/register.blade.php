6a78a7600ed30c150cfbb311652e2bcf8eba4e43
liberation_box <liberation.box@gmail.com>
2015-10-04 20:13:18 +0000

    added f5 style

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,47 +2,57 @@
 @section('title', 'Sign Up')
 
 @section('content')
-    <!-- resources/views/auth/register.blade.php -->
+    <div class="content">
+        <!-- resources/views/auth/register.blade.php -->
+    
+        @if (count($errors) > 0)
+            <div class="panel callout radius">
+                <div class="row">
+                    <div class="small-12 columns">
+                        <div class="alert alert-danger">
+                            <ul class="disc" data-errors>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+        
+        <!--
+            Forms & HTML
+            [http://laravelcollective.com/docs/5.1/html]
+        -->
+        <div class="panel radius">
+            <div class="row">
+                <div class="small-12 columns">
+                    {!! Form::open(['url' => '/auth/register']) !!}
+                        {!! Form::label('first_name', 'First Name') !!}
+                        {!! Form::text('first_name', old('first_name'), ['required' => 'required']) !!}
 
-    @if (count($errors) > 0)
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+                        {!! Form::label('last_name', 'Last Name') !!}
+                        {!! Form::text('last_name', old('last_name'), ['required' => 'required']) !!}
+
+                        {!! Form::label('gender', 'Gender') !!}
+                        {!! Form::select('gender', ['male' => 'Male', 'female' => 'Female'], old('gender'), ['placeholder' => 'Select your option', 'required' => 'required']) !!}
+
+                        {!! Form::label('email', 'Email') !!}
+                        {!! Form::email('email', old('email'), ['required' => 'required']) !!}
+
+                        {!! Form::label('password', 'Password') !!}
+                        {!! Form::password('password', ['required' => 'required']) !!}
+
+                        {!! Form::label('password_confirmation', 'Confirm Password') !!}
+                        {!! Form::password('password_confirmation', ['required' => 'required']) !!}
+
+                        <div class="clearfix">
+                            {!! Form::submit('Sign Up', ['class' => 'button radius small right ']) !!}
+                        </div>
+                    {!! Form::close() !!}
+                </div>
+            </div>
         </div>
-    @endif
-    
-    <!--
-        Forms & HTML
-        [http://laravelcollective.com/docs/5.1/html]
-    -->
-    {!! Form::open(array('url' => '/auth/register')) !!}
-        {!! Form::label('first_name', 'First Name') !!}
-        {!! Form::text('first_name', old('first_name')) !!}
-        <br>
-        
-        {!! Form::label('last_name', 'Last Name') !!}
-        {!! Form::text('last_name', old('last_name')) !!}
-        <br>
-        
-        {!! Form::label('gender', 'Gender') !!}
-        {!! Form::select('gender', ['male' => 'Male', 'female' => 'Female'], old('gender'), ['placeholder' => 'Select your option']) !!}
-        <br>
-        
-        {!! Form::label('email', 'Email') !!}
-        {!! Form::email('email', old('email')) !!}
-        <br>
-    
-        {!! Form::label('password', 'Password') !!}
-        {!! Form::password('password') !!}
-        <br>
-    
-        {!! Form::label('password_confirmation', 'Confirm Password') !!}
-        {!! Form::password('password_confirmation') !!}
-        <br>
-    
-        {!! Form::submit('Sign Up') !!}
-    {!! Form::close() !!}
+    </div>
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,12 +5,25 @@
         
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
         <link href="http://cdn.foundation5.zurb.com/foundation.css" rel="stylesheet" type="text/css">
-        <link href="css/app.css" rel="stylesheet" type="text/css">
+        <link href="/css/app.css" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div class="container">
-            @yield('content')
+            <nav class="top-bar" data-topbar role="navigation">
+              <ul class="title-area">
+                <li class="name">
+                  <h1><a href="#">Test task</a></h1>
+                </li>
+              </ul>
+            </nav>
+            <div class="row">
+                <div class="small-8 small-centered columns">
+                    @yield('content')
+                </div>
+            </div>
         </div>
+        
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
         <script type="text/javascript" src="http://cdn.foundation5.zurb.com/foundation.js"></script>
     </body>
 </html>
